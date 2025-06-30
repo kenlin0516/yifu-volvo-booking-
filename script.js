b630597b3f70058ec0f1fa5baf154211f9b9792f
yifu <63226445+kenlin0516@users.noreply.github.com>
2025-06-30 23:35:21 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
 
-document.getElementById("bookingForm").addEventListener("submit", function(e) {
+document.getElementById("booking-form").addEventListener("submit", function(e) {
   e.preventDefault();
-
-  const data = {
-    name: document.getElementById("name").value,
-    phone: document.getElementById("phone").value,
-    carType: document.getElementById("carType").value,
-    plate: document.getElementById("plate").value,
-    service: document.getElementById("service").value,
-    date: document.getElementById("date").value,
-    time: document.getElementById("time").value,
-  };
-
-  fetch("https://script.google.com/macros/s/AKfycbzRxeSco9StkiQomTxk_6vsgB0K1Y1fPm6yQyjrpXJc-LIYt0wz0-Y08imkrpCdnW5e/exec", {
-    method: "POST",
-    mode: "no-cors",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data)
-  }).then(() => {
-    alert("預約成功！");
-    document.getElementById("bookingForm").reset();
-  }).catch(() => {
-    alert("發送失敗，請稍後再試！");
-  });
+  alert("預約成功！");
 });
