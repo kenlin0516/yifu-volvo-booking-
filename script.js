ee7588af8072eeb675c81776a3bd53be1e64cf3b
yifu <63226445+kenlin0516@users.noreply.github.com>
2025-07-01 00:06:29 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-
-document.getElementById("booking-form").addEventListener("submit", function(e) {
+document.getElementById('booking-form').addEventListener('submit', function(e) {
   e.preventDefault();
-  alert("預約成功！");
+  const formData = new FormData(this);
+  const obj = Object.fromEntries(formData.entries());
+
+  fetch("https://script.google.com/macros/s/AKfycbzRxeSco9StkiQomTxk_6vsgB0K1Y1fPm6yQyjrpXJc-LIYt0wz0-Y08imkrpCdnW5e/exec", {
+    method: "POST",
+    body: JSON.stringify(obj),
+    headers: {
+      "Content-Type": "application/json",
+    }
+  })
+  .then(res => res.text())
+  .then(text => {
+    document.getElementById("success-message").style.display = "block";
+    this.reset();
+  })
+  .catch(err => alert("發送失敗，請稍後再試"));
 });
